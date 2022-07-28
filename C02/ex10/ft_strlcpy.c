82786ad290e0316273c8f9c0eb559966616b5a9e
zeynep şaşmaz <102350433+zeynepssasmaz@users.noreply.github.com>
2022-07-28 13:30:46 +0300

    Update ft_strlcpy.c

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -1,15 +1,30 @@
 
 
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
+	unsigned int	a;
 
+	a = ft_strlen(src);
 	i = 0;
-	while (src[i] && i < size)
+	if (size != 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while (src[i] != '\0' && i < size -1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	dest[i] = '\0';
-	return (i);
+	return (a);
 }
