e5dc998460c47b73251b013f3b49475151a7d5b2
zeynep şaşmaz <102350433+zeynepssasmaz@users.noreply.github.com>
2022-07-28 13:28:43 +0300

    Create ft_strncpy.c

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -1,14 +1,17 @@
-
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
 
 	i = 0;
-	while (src[i] && i < n)
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
