2ff0a81ceb33b1b0d4d1fd97bea5b1265a4ad4a5
zeynep şaşmaz <102350433+zeynepssasmaz@users.noreply.github.com>
2022-08-28 19:22:39 +0300

    Update ft_rev_params.c

--- C06/ex02/ft_rev_params.c
+++ C06/ex02/ft_rev_params.c
@@ -1,35 +1,33 @@
-
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zsasmaz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 20:06:29 by zsasmaz           #+#    #+#             */
+/*   Updated: 2022/02/27 20:06:34 by zsasmaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
+int	main(int ac, char **av)
 {
 	int	i;
+	int	j;
 
-	i = 0;
-	while (str[i])
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
-}
-
-int	main(int argc, char **argv)
-{
-	int	i;
-
-	i = argc - 1;
-	if (argc > 1)
+	i = ac - 1;
+	if (ac > 1)
 	{
 		while (i > 0)
 		{
-			ft_putstr(argv[i]);
-			ft_putchar('\n');
+			j = 0;
+			while (av[i][j] != '\0')
+			{
+				write(1, &av[i][j], 1);
+				j++;
+			}
+			write(1, "\n", 1);
 			i--;
 		}
 	}
