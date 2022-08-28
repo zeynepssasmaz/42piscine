9bb36097741fc87a68359b68ce7cfe7320b7e891
zeynep şaşmaz <102350433+zeynepssasmaz@users.noreply.github.com>
2022-08-28 19:17:34 +0300

    Update ft_recursive_factorial.c

--- C05/ex01/ft_recursive_factorial.c
+++ C05/ex01/ft_recursive_factorial.c
@@ -1,10 +1,19 @@
-
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zsasmaz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 19:39:10 by zsasmaz           #+#    #+#             */
+/*   Updated: 2022/02/27 19:39:17 by zsasmaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 int	ft_recursive_factorial(int nb)
-{	
+{
 	if (nb < 0)
 		return (0);
-	if (nb == 0 || nb == 1)
-		return (1);
-	return (nb * ft_recursive_factorial(nb - 1));
+	if (nb > 0)
+		return (nb * ft_recursive_factorial(nb - 1));
+	return (1);
 }
