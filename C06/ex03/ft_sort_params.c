442ac60cf4edbf7d9af631a6744ff9d54cd38e5c
zeynep şaşmaz <102350433+zeynepssasmaz@users.noreply.github.com>
2022-08-28 19:23:19 +0300

    Update ft_sort_params.c

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -1,5 +1,14 @@
-
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zsasmaz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 20:06:47 by zsasmaz           #+#    #+#             */
+/*   Updated: 2022/02/27 20:06:52 by zsasmaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -13,10 +22,7 @@ void	ft_putstr(char *str)
 
 	i = 0;
 	while (str[i])
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
+		ft_putchar(str[i++]);
 }
 
 int	ft_strcmp(char *s1, char *s2)
@@ -24,40 +30,48 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 
 	i = 0;
-	while (s1[i] || s2[i])
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] == s2[i])
+			i++;
+		else if (s1[i] < s2[i] || s1[i] > s2[i])
 			return (s1[i] - s2[i]);
-		i++;
 	}
 	return (0);
 }
 
-int	main(int argc, char **argv)
+void	ft_swap(char **a, char **b)
 {
-	int		i;
 	char	*temp;
 
-	i = 1;
-	if (argc > 1)
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int	main(int ac, char **av)
+{
+	int	x;
+	int	j;
+
+	x = 1;
+	while (x < ac - 1)
 	{
-		while (argv[i] && argv[i + 1])
+		j = 1;
+		while (j < ac - 1)
 		{
-			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
-			{
-				temp = argv[i];
-				argv[i] = argv[i + 1];
-				argv[i + 1] = temp;
-				i = 0;
-			}
-			i++;
+			if (ft_strcmp(av[j], av[j + 1]) > 0)
+				ft_swap(&av[j], &av[j + 1]);
+			j++;
 		}
+		x++;
 	}
-	i = 0;
-	while (argv[++i])
+	j = 1;
+	while (j < ac)
 	{
-		ft_putstr(argv[i]);
+		ft_putstr(av[j]);
 		ft_putchar('\n');
+		j++;
 	}
 	return (0);
 }
