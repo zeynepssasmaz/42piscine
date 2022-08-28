144230d77088fd796486ebbb1dacef9a9cb3b5e2
zeynep şaşmaz <102350433+zeynepssasmaz@users.noreply.github.com>
2022-08-28 19:20:56 +0300

    Update ft_find_next_prime.c

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -1,24 +1,33 @@
-
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zsasmaz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 19:59:26 by zsasmaz           #+#    #+#             */
+/*   Updated: 2022/02/27 19:59:31 by zsasmaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 int	ft_is_prime(int nb)
 {
-	int	i;
+	int	n;
 
-	i = 2;
+	n = 2;
 	if (nb < 2)
 		return (0);
-	while (i < nb)
+	while (n < nb)
 	{
-		if (!(nb % i))
+		if (nb % n == 0)
 			return (0);
-		i++;
+		n++;
 	}
 	return (1);
 }
 
 int	ft_find_next_prime(int nb)
 {
-	if (!(ft_is_prime(nb)))
-		return (ft_find_next_prime(++nb));
+	while (nb < 2147483647 && !ft_is_prime(nb))
+		nb++;
 	return (nb);
 }
